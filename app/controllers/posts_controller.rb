80e1dbbcf3882c1289be9c966c1d8ef5e0fcb236
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-02 05:56:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,8 @@ class PostsController < ApplicationController
 	end
 	private
  
-def post_params
-  params.require(:post).permit(:title, :description)
-end
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
 end
